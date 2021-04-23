42f2a69e2c52d8ffe4fd16f2630cfbcdf46ea755
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-04-23 04:24:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let nums=function(num){
-    var val = document.getElementById('box1').textContent;
-
-    if (val == 0) {
+    var valBox2 = document.getElementById('box2').textContent;
+    var valBox1 = document.getElementById('box1').textContent;
+    var valBox3 = document.getElementById('box3').textContent
+    if (valBox2 == '') {
         let box1=document.getElementById("box1");
-        box1.innerHTML=num;
+        box1.innerHTML= valBox1+=num ;
     }
     else{
         let box3=document.getElementById("box3");
-        box3.innerHTML=num; 
+        box3.innerHTML= valBox3+=num ; 
     }
 }
 
@@ -15,6 +16,34 @@ let opr =function(op){
 
     let box2=document.getElementById("box2");
     box2.innerHTML=op;
+}
 
+let del =function(){
+    box1.innerHTML='';
+    box2.innerHTML='';
+    box3.innerHTML='';
+}
+
+let eq = function(){
+
+    var n1 = document.getElementById('box1').textContent;
+    var _op = document.getElementById('box2').textContent;
+    var n2 = document.getElementById('box3').textContent;
+    if (_op == '+'){
+        a = parseInt(n1) + parseInt(n2);
+    }
+    else if (_op == '-') {
+        a = parseInt(n1) - parseInt(n2);
+    }
+    else if (_op == '*') {
+        a = parseInt(n1) * parseInt(n2);
+    }
+    else if (_op == '/') {
+        a = parseInt(n1) / parseInt(n2);
+    }
+    
+
+    ansLable.innerHTML=a;
+    
 }
 
